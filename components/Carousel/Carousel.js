a1d48ffebbf39d06b8290d7432044ccff58a9878
Adonis Puente <adonispuente@Adoniss-Air.attlocal.net>
2020-04-10 12:46:58 -0400

    attempted stretch

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -17,3 +17,48 @@
     <div class="right-button"> > </div>
   </div>
 */
+
+pictures = [
+  "./assets/carousel/mountains.jpeg",
+  "./assets/carousel/computer.jpeg",
+  "./assets/carousel/trees.jpeg",
+  "./assets/carousel/turntable.jpeg",
+];
+function carousel(item) {
+  const container = document.createElement("div");
+  container.classList.add("carousel");
+
+  const div = document.createElement("div");
+  div.classList.add("left-button");
+  container.appendChild(div);
+
+  div.addEventListener("click", (event) => {});
+
+  const pic = document.createElement("img");
+  pic.src = item;
+  container.appendChild(pic);
+
+  const rightBttn = document.createElement("right-button");
+  rightBttn.classList.add("right-button");
+  container.appendChild(rightBttn);
+
+  return container;
+}
+
+pictures.forEach((item) => {
+  carousel(item);
+
+  document.querySelector(".carousel-container").appendChild(carousel(item));
+});
+
+// const pic2 = document.createElement("img");
+// pic2.src = "./assets/carousel/computer.jpeg";
+// container.appendChild(pic2);
+
+// const pic3 = document.createElement("img");
+// pic3.src = "./assets/carousel/trees.jpeg";
+// container.appendChild(pic3);
+
+// const pic4 = document.createElement("img");
+// pic4.src = "./assets/carousel/turntable.jpeg";
+// container.appendChild(pic4);
